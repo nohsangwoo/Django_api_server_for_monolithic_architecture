76083b095cd1f5ae84701a7ac1e50ddf0a86b496
nohsangwoo <nsgr12@naver.com>
2022-06-02 17:54:38 +0900

    set basic form that GET and POST method on shop model

--- README.md
+++ README.md
@@ -7,4 +7,13 @@
 - ref: https://www.django-rest-framework.org/#quickstart
 
 # serializer?
+
 - DB data를 json 형태로 변환 시켜주는것
+
+# create GET, POST api and test with POSTMAN
+
+ex)
+
+```
+http://127.0.0.1:8000/order/shops
+```
--- order/views.py
+++ order/views.py
@@ -4,13 +4,20 @@ from django.shortcuts import render
 from django.views.decorators.csrf import csrf_exempt
 from order.models import Shop, Menu, Order, Orderfood
 from order.serializers import ShopSerializer
+from rest_framework.parsers import JSONParser
 
 
 @csrf_exempt
 def shop(request):
-    if request.methods == 'GET':
+    if request.method == 'GET':
         shop = Shop.objects.all()
         serializer = ShopSerializer(shop, many=True)
         return JsonResponse(serializer.data, safe=False)
 
-    # elif request.methods == 'POST':
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = ShopSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
